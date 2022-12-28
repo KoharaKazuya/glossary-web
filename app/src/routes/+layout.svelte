b47f2931c6447c5274b063c1b755ecaab0432148
KoharaKazuya <reflect.results@gmail.com>
2022-12-28 22:57:11 +0900

    tmp

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
+	import Header from '$lib/Header.svelte';
 	import 'sanitize.css';
 </script>
 
+<Header />
 <slot />
---
+++ app/src/routes/g/[glossaryId]/[term]/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	let html = '';
+</script>
+
+<nav>
+	<a href={`/g/${data.glossaryId}`}>{data.glossaryId}</a>
+</nav>
+
+<h1>{data.term}</h1>
+
+<textarea bind:value={html} />
+
+<div>{@html html}</div>
