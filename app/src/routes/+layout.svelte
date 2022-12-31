9cdc5d4a298e74796acf7d5902596e5e6be32521
KoharaKazuya <reflect.results@gmail.com>
2022-12-31 07:58:33 +0000

    tmp

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-	import TopAppBar, { AutoAdjust, Row, Section, Title } from '@smui/top-app-bar';
+	import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
 	import 'sanitize.css';
-
-	let topAppBar: TopAppBar;
 </script>
 
-<TopAppBar bind:this={topAppBar} variant="standard"
+<TopAppBar variant="static"
 	><Row>
 		<Section align="start"><a href="/"><Title>glossary-web</Title></a></Section>
 		<Section><a href="/g/new">用語集を作る</a></Section>
 	</Row>
 </TopAppBar>
 
-<AutoAdjust {topAppBar}><slot /></AutoAdjust>
+<main><slot /></main>
